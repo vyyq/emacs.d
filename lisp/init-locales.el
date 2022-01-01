b6ac3e9667efa1a781820382fcb2420c398e1c71
yanyuqi2 <yuqin.yan@mail.utoronto.ca>
2022-01-01 15:35:33 -0500

    Update personal configs in lisp/init-locales.el.

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -22,5 +22,50 @@
 (unless (eq system-type 'windows-nt)
   (set-selection-coding-system 'utf-8))
 
+
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+;; (package-initialize)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+(unless (package-installed-p 'key-chord)
+  (package-install 'key-chord))
+
+(require 'key-chord)
+(key-chord-mode 1)
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
+
+;; sudo apt-get install texlive texlive-xetex texlive-latex-extra
+;; sudo apt-get install latex-cjk-all
+
+;; add to org header
+;; #+LATEX_HEADER: \usepackage{xeCJK}
+;; #+LATEX_HEADER: \setCJKmainfont{SimSun}
+;; or
+;; #+LATEX_HEADER: \usepackage{ctex}
+
+(require 'org)
+;;  org-mode 8.0
+(setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
+                              "xelatex -interaction nonstopmode %f"))
+
+;; export cn character
+(setf org-latex-default-packages-alist
+      (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+
+(load-theme 'wombat t)
+
 (provide 'init-locales)
 ;;; init-locales.el ends here
