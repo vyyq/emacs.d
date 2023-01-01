5eaafb9ce00d2d2336eaf43ace165a88b3d71001
Yuqin Yan <yuqin.yan@mail.utoronto.ca>
2023-01-01 13:36:04 -0500

    Update init-locale.el

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -386,11 +386,33 @@
 
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
-;; (unless (package-installed-p 'emojify)
-  ;; (package-install 'emojify'))
+(unless (package-installed-p 'dired-sidebar)
+  (package-install 'dired-sidebar))
 
-(use-package emojify
-  :hook (after-init . global-emojify-mode))
+(unless (package-installed-p 'vscode-icon)
+  (package-install 'vscode-icon))
+
+(use-package vscode-icon
+  :ensure t
+  :commands (vscode-icon-for-file))
+
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
 
 (provide 'init-locales)
 ;;; init-locales.el ends here
